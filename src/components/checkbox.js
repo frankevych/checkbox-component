3eac65e1f7797a719787744ebd71e18161571979
Stepan Frank <stepanfrankevych@gmail.com>
2019-03-28 23:20:29 +0200

    created checkbox component

---
+++ src/components/checkbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Checkbox = () => {
+    return (
+        <div className='checkbox-li'>
+            <input type="checkbox"/>
+            <span>English</span>
+        </div>
+    );
+}
+
+export default Checkbox;
